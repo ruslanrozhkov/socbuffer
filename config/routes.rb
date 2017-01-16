fb24c55fe2c7fc2cb7ae08af8f1fafc53fe654b1
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-16 14:58:09 +0200

    Added cancel posts option

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
 
   get 'dashboard', to: 'pages#dashboard'
 
-  resources :posts, only: [:new, :create]
+  resources :posts do
+    member do
+      put :cancel
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
